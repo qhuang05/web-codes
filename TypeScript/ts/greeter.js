aef9939cb57608a1cfd21cb83d8c0f784ace0faa
qhuang05 <qhuang05@sina.com>
2020-07-14 19:58:24 +0800

    update

--- TypeScript/ts/greeter.js
+++ TypeScript/ts/greeter.js
@@ -1,18 +1,19 @@
+// function greeter(person: string){
+//     return 'Hello, ' + person
+// }
+// let user = 'World';
+// document.body.innerHTML = greeter(user);
+// // tsc greeter.ts   报错
 var Student = /** @class */ (function () {
     function Student(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.fullName = firstName + ' ' + lastName;
+        this.fullName = firstName + " " + lastName;
     }
     return Student;
 }());
 function greeter(person) {
-    return "Hello, " + person.firstName + ' ' + person.lastName;
-    // return "Hello, " + person.fullName;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-// let user = "Jane User";
-// let user = ["Jane User", "Mary"];
-// let user = {firstName: "Jane", lastName: 'User'};
 var user = new Student('Jane', 'User');
-console.log(user);
 document.body.innerHTML = greeter(user);
--- TypeScript/ts/greeter.ts
+++ TypeScript/ts/greeter.ts
@@ -1,24 +1,25 @@
+// function greeter(person: string){
+//     return 'Hello, ' + person
+// }
+// let user = 'World';
+// document.body.innerHTML = greeter(user);
+// // tsc greeter.ts   报错
+
+
+
 class Student {
-    fullName: String;
-    constructor(firstName, lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.fullName = firstName + ' ' + lastName;
+    fullName: string;
+    constructor(public firstName, public lastName) {
+        this.fullName = `${firstName} ${lastName}`;
     }
 }
-
-interface Person {
-    firstName: String;
-    lastName: String;
+interface Person{
+    firstName: string;
+    lastName: string;
 }
-function greeter(person:Person){
-    return "Hello, " + person.firstName + ' ' + person.lastName;
-    // return "Hello, " + person.fullName;
+function greeter(person: Person){
+    return `Hello, ${person.firstName} ${person.lastName}`;
 }
-// let user = "Jane User";
-// let user = ["Jane User", "Mary"];
-// let user = {firstName: "Jane", lastName: 'User'};
 let user = new Student('Jane', 'User');
-console.log(user)
-
 document.body.innerHTML = greeter(user);
+
